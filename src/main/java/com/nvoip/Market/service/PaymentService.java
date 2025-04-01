e5271396e95085c1a29ebb3145efd855e4c96588
oricardoz <ricardoapzaghetto@gmail.com>
2025-04-01 18:51:18 -0300

    Fixing import

--- src/main/java/com/nvoip/Market/service/PaymentService.java
+++ src/main/java/com/nvoip/Market/service/PaymentService.java
@@ -7,21 +7,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.mercadopago.MercadoPagoConfig;
-import com.mercadopago.client.common.AddressRequest;
-import com.mercadopago.client.common.IdentificationRequest;
-import com.mercadopago.client.common.PhoneRequest;
 import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
 import com.mercadopago.client.preference.PreferenceClient;
 import com.mercadopago.client.preference.PreferenceDifferentialPricingRequest;
-import com.mercadopago.client.preference.PreferenceFreeMethodRequest;
 import com.mercadopago.client.preference.PreferenceItemRequest;
-import com.mercadopago.client.preference.PreferencePayerRequest;
-import com.mercadopago.client.preference.PreferencePaymentMethodRequest;
-import com.mercadopago.client.preference.PreferencePaymentMethodsRequest;
-import com.mercadopago.client.preference.PreferencePaymentTypeRequest;
-import com.mercadopago.client.preference.PreferenceReceiverAddressRequest;
 import com.mercadopago.client.preference.PreferenceRequest;
-import com.mercadopago.client.preference.PreferenceShipmentsRequest;
 import com.mercadopago.resources.preference.Preference;
 import com.mercadopago.exceptions.MPException;
 import com.mercadopago.exceptions.MPApiException;
@@ -31,7 +21,7 @@ public class PaymentService {
 
     public void processPayment() {
         
-        MercadoPagoConfig.setAccessToken("TEST-7123191147819459-033119-ee94285f916d3b36ab3863ed284bd65b-1107604901");
+        MercadoPagoConfig.setAccessToken("your-code");
         PreferenceClient client = new PreferenceClient();
         
         PreferenceItemRequest itemRequest =
